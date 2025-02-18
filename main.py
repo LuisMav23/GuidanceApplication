1512f6d6dd3d6166770f7731d008639da6578893
gabrielluismaverick <gabrielluismaverick@gmail.com>
2025-02-18 05:07:08 +0000

    asdf

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello_world():
   """Example Hello World route."""
   name = os.environ.get("NAME", "World")
